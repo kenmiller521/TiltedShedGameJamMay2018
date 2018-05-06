25aa56315eff780ecc6a92d343267e201a3d2b66
Joel Lee <lee.joel395@gmail.com>
2018-05-06 16:08:49 -0700

    NetWorking experiments

---
+++ Assets/_NetworkedAssets/PlayerPickUpHandler_NET.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+/// <summary>
+/// handles the collectables for each player
+/// Joel Lee
+/// </summary>
+public class PlayerPickUpHandler_NET : NetworkBehaviour {
+    [SyncVar]
+    private int _pickUpCount;
+    [SyncVar]
+    ChangeSizeEvent AdjustSpeed;
+
+    void Start() {
+        AdjustSpeed = new ChangeSizeEvent();
+    }
+
+    public int PickUpCount {
+        get { return _pickUpCount; }
+        set { _pickUpCount = value; }
+    }
+    /// <summary>
+    /// Changes the amount of pickups we are carrying up to an amount
+    /// Fires off the adjust speed event on the player
+    /// recieves the change size event from the player
+    /// </summary>
+    /// <param name="amount"></param>
+    public void ChangeSize(int amount) {
+        _pickUpCount -= amount;
+        AdjustSpeed.Invoke(_pickUpCount);
+    }
+
+}
