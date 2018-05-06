c43d2b40496bef5b15cabcde87ee5ce7c2860209
Ruben Sanchez <SanchezVRuben@gmail.com>
2018-05-06 15:48:53 -0700

    started collision manager

--- Assets/_Scripts/CollisionManager.cs
+++ Assets/_Scripts/CollisionManager.cs
@@ -8,15 +8,35 @@
 
 public class CollisionManager : MonoBehaviour
 {
+    public static float forceScaleMultiplier = 2f;
+
     private Rigidbody2D rigidB;
+
+    private PlayerPickUpHandler pickUpHandler;
 
     private void Awake()
     {
         rigidB = GetComponentInParent<Rigidbody2D>();
+        pickUpHandler = GetComponent<PlayerPickUpHandler>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        throw new System.NotImplementedException();
+        if (other.gameObject.GetComponent<PlayerPickUpHandler>() != null)
+        {
+            PlayerPickUpHandler otherPickUpHandler = other.gameObject.GetComponent<PlayerPickUpHandler>();
+
+            //if other planet is smaller
+            if (otherPickUpHandler.PickUpCount < pickUpHandler.PickUpCount)
+            {
+                int difference = pickUpHandler.PickUpCount - otherPickUpHandler.PickUpCount;
+                Vector2 direction = Vector3.Normalize(other.transform.position - transform.position);
+
+                otherPickUpHandler.GetComponentInParent<Rigidbody2D>().AddForce(direction * forceScaleMultiplier);
+            }
+
+
+        }
+
     }
 }
