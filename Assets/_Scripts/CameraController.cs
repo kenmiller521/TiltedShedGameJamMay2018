6413af3ebeefc8e1aa52acdccc0bee66fa0ed32e
Paper-Hat <justinkristian@gmail.com>
2018-05-06 12:49:53 -0700

    BasicCamera

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.LuisPedroFonseca.ProCamera2D;
+/// <summary>
+/// To access camera, use "ProCamera2D.Instance", then your methods/functions
+/// </summary>
+public class CameraController : MonoBehaviour {
+    public GameObject[] players;
+	// Use this for initialization
+	void Start () {
+		foreach(GameObject player in players)
+        {
+            ProCamera2D.Instance.AddCameraTarget(player.transform, 1f, 1f, 0f);
+        }
+	}
+}
