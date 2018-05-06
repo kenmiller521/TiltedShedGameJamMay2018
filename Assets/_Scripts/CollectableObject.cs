1a77e065e2c43764e0a2a9c448931a6cf64edca5
Joel Lee <lee.joel395@gmail.com>
2018-05-06 12:46:18 -0700

    CollectablesBasis

---
+++ Assets/_Scripts/CollectableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableObject : MonoBehaviour { 
+    public delegate void OnPickUpEvent();
+    public event OnPickUpEvent OnPickUp;
+
+    void OnCollisionEnter2D(Collision2D c) {
+        PlayerPickUpHandler temp = c.gameObject.GetComponent<PlayerPickUpHandler>();
+        if (temp != null) {
+            temp.IncrementCount();
+            OnPickUp();
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerPickUpHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerPickUpHandler : MonoBehaviour {
+    private int PickUpCount;
+    //playercontroller
+
+    public void IncrementCount() {
+        PickUpCount++;
+    }
+
+    public void DecrementSize() {
+        PickUpCount--;
+    }
+
+    public void AdjustPlayer() {
+        //playercontroller.changestats(pickupcount)
+    }
+
+}
