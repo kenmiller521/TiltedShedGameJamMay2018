c11902b95841331e49b07075862fed4de950045d
Paper-Hat <justinkristian@gmail.com>
2018-05-07 21:32:42 -0700

    "Lerp" of player size on size increase/decrease

--- Assets/_Scripts/PlayerSize.cs
+++ Assets/_Scripts/PlayerSize.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-
+using System.Collections;
 /// <summary>
 /// Manages player size with local scale
 ///
@@ -9,14 +9,17 @@
 
 public class PlayerSize : MonoBehaviour
  {
-     [SerializeField]
-     private Vector2 startingLocalScale = Vector2.one;
-
-     [SerializeField]
-     private Vector2 minLocalScale = Vector2.one;
+    [SerializeField]
+    private Vector2 startingLocalScale = Vector2.one;
 
     [SerializeField]
-     private float scaleChangeSize = .5f;
+    private Vector2 minLocalScale = Vector2.one;
+
+    [SerializeField]
+    private float scaleChangeSize = .5f;
+
+    [SerializeField]
+    private float timeToChange = 1f;
 
      private void Awake()
      {
@@ -25,16 +28,37 @@ public class PlayerSize : MonoBehaviour
 
      public void IncreaseSize()
      {
-        transform.localScale = new Vector2(transform.localScale.x + scaleChangeSize, transform.localScale.y + scaleChangeSize);
+        Coroutine increase = StartCoroutine(IIncreasable());
      }
 
      public void DecreaseSize()
      {
-         transform.localScale = new Vector2(transform.localScale.x - scaleChangeSize, transform.localScale.y - scaleChangeSize);
-
-         if (transform.localScale.magnitude < minLocalScale.magnitude)
-             transform.localScale = minLocalScale;
+        Coroutine decrease = StartCoroutine(IDecreasable());
      }
 
+    public IEnumerator IDecreasable()
+    {
+        Vector2 targetSize = new Vector2(transform.localScale.x - scaleChangeSize, transform.localScale.y - scaleChangeSize);
+        if (transform.localScale.magnitude >= minLocalScale.magnitude)
+        {
+            while (transform.localScale.magnitude >= targetSize.magnitude)
+            {
+                if (transform.localScale.magnitude < minLocalScale.magnitude){
+                    transform.localScale = minLocalScale;
+                    break;
+                }
+                transform.localScale = new Vector2(transform.localScale.x - .05f, transform.localScale.y - 0.05f);
+                yield return new WaitForSeconds(.01f);
+            }
+        }
+    }
+    public IEnumerator IIncreasable()
+    {
+        Vector2 targetSize = new Vector2(transform.localScale.x + scaleChangeSize, transform.localScale.y + scaleChangeSize);
+        while (transform.localScale.magnitude <= targetSize.magnitude){
+            transform.localScale = new Vector2(transform.localScale.x +.05f, transform.localScale.y + 0.05f);
+            yield return new WaitForSeconds(.01f);
+        }
+    }
 
- }
+}
